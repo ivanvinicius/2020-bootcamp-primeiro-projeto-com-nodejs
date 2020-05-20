4f28a8727fe35d4b61e6920f6c99a13b03b45a6b
ivanvinicius <ivanvinicius88@gmail.com>
2020-05-20 09:50:11 -0300

    configurando estrutura

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+function logRequest(request: Request, response: Response, next: NextFunction): void {
+  const { method, url } = request;
+
+  const log: string = `[${method.toUpperCase()}] ${url}`;
+  
+  console.time(log)
+
+  next();
+
+  console.timeEnd(log);
+}
+
+app.use(express.json());
+app.use(logRequest);
+
+app.get('/', (request, response) => response.send('hello baby'));
+
+app.listen(3333, () => console.log('🚀 Server is Running on port:3333'))
