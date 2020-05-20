47ba46b9c983de181301d5ee222bc2bf6f37b83a
ivanvinicius <ivanvinicius88@gmail.com>
2020-05-20 11:16:34 -0300

    importando arquivos .TS

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,20 @@
+/* eslint-disable no-console */
 import express, { Request, Response, NextFunction } from 'express';
+
+import routes from './routes';
 
 const app = express();
 
 function logRequest(request: Request, response: Response, next: NextFunction): void {
-  const { method, url } = request;
-
-  const log: string = `[${method.toUpperCase()}] ${url}`;
+  const log: string = `[${request.method.toUpperCase()}] ${request.url}`;
 
   console.time(log);
-
   next();
-
   console.timeEnd(log);
 }
 
 app.use(express.json());
 app.use(logRequest);
-
-app.get('/', (request, response) => response.send('hello baby'));
+app.use(routes);
 
 app.listen(3333, () => console.log('🚀 Server is Running on port:3333'));
